eb77c539b1181940f33f4a99de840527dd0603f6
Sourav Walke <sourav.wal@gmail.com>
2024-05-16 14:02:20 -0700

    Initial Setup
    Starter files

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Header from "@/components/header";
+import React from 'react';
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Sourav Walke | Personal Portfolio',
+  description: 'Sourav is a full-stack developer with 3 years of experience.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={'${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36'}>
+        <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75]'></div>
+        <div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] '></div>
+        
+        <Header />
+        {children}
+      </body>
+    </html>
+  )
+}
