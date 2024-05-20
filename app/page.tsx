46c7da2699b11659bc51988f930619ed48261a91
Sourav Walke <sourav.wal@gmail.com>
2024-05-19 22:49:03 -0700

    Added Skills and Experience Section

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import Intro from "@/components/intro";
 import SectionDivider from "@/components/section-divider";
 import About from "@/components/about";
 import Skills from "@/components/skills";
+import Experience from "@/components/experience";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center">
+    <main className="flex flex-col items-center px-4">
       <Intro />
       <SectionDivider/>
       <About />
       <Skills />
+      <Experience />
     </main>
-  )
+  );
 }
