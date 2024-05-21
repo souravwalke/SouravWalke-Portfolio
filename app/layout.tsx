9c7cb96c9e7d5e666fb5fd4e74ef89eb293f123a
Sourav Walke <sourav.wal@gmail.com>
2024-05-21 02:48:44 -0700

    Added Experience Timeline data

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className='!scroll-smooth'>
       <body className={'${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36'}>
-        <div className='bg-[#FFBBBB] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75]'></div>
-        <div className='bg-[#A9F1DF] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] '></div>
+        <div className='bg-[#FCB69F] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75]'></div>
+        <div className='bg-[#FFECD2] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] '></div>
         <ActiveSectionContextProvider>
           <Header />
           {children}
